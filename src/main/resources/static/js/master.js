a38a0f8945273c56cbb1d44318943a818aa04bf2
xuexiangyunb <179336284@qq.com>
2019-05-07 17:07:33 +0800

    瞅一眼

--- src/main/resources/static/js/master.js
+++ src/main/resources/static/js/master.js
@@ -4,6 +4,8 @@ $(
 
 
 
+
+
 //加载初始化数据
 function initData()
 {
@@ -13,8 +15,25 @@ function initData()
         type : "get",//请求方式
         success : function(result) {  //如果请求成功，返回数据。
             $("#h6").text(result.humData)
-            $("#h8").text(result.temData)
+            if (result.humData>70&&result.humData<75 ){
 
-        },
+             alert("湿度异常预警");
+
+}          else if (result.humData>75)
+            {  alert("湿度异常");
+
+
+}
+
+            $("#h8").text(result.temData)
+            if (result.temData>90&&result.temData<95 ){
+                alert("温度异常预警")}
+
+            else if (result.temData>75)
+            {
+                alert("温度异常");
+            }
+
+            },
     })
 }
