9726cd0134e89fa41df70b42041e1a0cf5c1ded1
xuexiangyunb <179336284@qq.com>
2019-05-09 16:43:34 +0800

    gaojing

--- src/main/resources/static/js/master.js
+++ src/main/resources/static/js/master.js
@@ -2,63 +2,42 @@ $(
     initData()
 )
 
-
-
-
-
 //加载初始化数据
 function initData()
 {
-    $.ajax({
-        url : "listHT",//后台请求的数据，
-        dataType : "json",//数据格式
-        type : "get",//请求方式
-        success : function(result) {  //如果请求成功，返回数据。
+    $.ajax
+    ({
+        url: "listHT",//后台请求的数据，
+        dataType: "json",//数据格式
+        type: "get",//请求方式
+        success: function (result) {  //如果请求成功，返回数据。
             $("#h10").text(result.timeData)
             $("#h6").text(result.humData)
-            if (result.humData>70&&result.humData<75 ){
-
-             alert("湿度异常预警");
-
-}          else if (result.humData>75)
-            {
-                alert("湿度异常");
-                $.ajax({
-                    dataType : "json",
-                    url:"addWarning",
-                    type:"get",
-                    success:function (result) {
-
-                        
-                    }
-
-
-                })
-
-
-}
-
             $("#h8").text(result.temData)
-            if (result.temData>90&&result.temData<95 ){
-                alert("温度异常预警")}
+            if (result.humData > 70 && result.humData < 75 || result.temData > 90 && result.temData < 95) {
 
-            else if (result.temData>75)
-            {
-                alert("温度异常");
+                alert("温湿度异常预警");
+            }
+            else if (result.humData > 75 || result.temData > 75) {
 
                 $.ajax({
-                    dataType : "json",
-                    url:"addWarning",
-                    type:"get",
-                    success:function (result) {
+                    dataType: "json",
+                    url: "/addWarning",
+                    type: "get",
+
+                    success: function (data) {
 
 
                     }
 
 
                 })
-            }
 
-       },
+                alert("湿度异常");
+            }
+        }
+
     })
 }
+
+
